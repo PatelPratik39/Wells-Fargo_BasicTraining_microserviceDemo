f0c5d162e81aab910ce80e4f770505893aa73b32
PatelPratik39 <ptl.pratik717@gmail.com>
2025-04-17 17:58:37 -0500

    kafka configuration - Partial

--- userService/src/main/java/com/wf/userservice/kafka/KafkaProducerService.java
+++ userService/src/main/java/com/wf/userservice/kafka/KafkaProducerService.java
@@ -8,7 +8,7 @@ public class KafkaProducerService {
 
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    private final String TOPIC = "user-topic";
+    private final String TOPIC = "user-Microservice";
 
     public void sendMessage(String message) {
         kafkaTemplate.send(TOPIC, message);
