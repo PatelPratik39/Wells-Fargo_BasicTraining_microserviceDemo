9c78f887fce6c3b50305f45582091efdbc0e1ab1
PatelPratik39 <ptl.pratik717@gmail.com>
2025-04-18 15:29:39 -0500

    Product service is dockerized now

---
+++ productService/src/main/java/com/wf/productservice/ProductServiceApplication.java
@@ -0,0 +1,14 @@
+package com.wf.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+		System.err.println("Product Service is running... 🖥️ ");
+	}
+
+}
---
+++ productService/src/test/java/com/wf/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wf.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
